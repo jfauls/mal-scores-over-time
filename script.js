8e006d9e2be78fb9c158095799039043b6f87452
Joseph Fauls <hnuisqt@gmail.com>
2023-03-29 18:38:39 -0400

    json format change

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ fetch("https://jfauls.github.io/mal-scores-over-time/SeasonalAnime.json")
   .then((data) => {
     var news = document.getElementsByClassName("news-story")[0];
     var items = data;
-    console.log(data)
-    for(var i = 0; i < items.length; i++) {
+    console.log(data);
+    console.log(Object.keys(items));
+    keys = Object.keys(items);
+    console.log(items[keys[i]].score[0])
+    for(var i = 0; i < keys.length; i++) {
         var h5 = document.createElement("h5");
-        h5.innerHTML = items[i].title;
+        h5.innerHTML = keys[i];
         news.appendChild(h5);
         var p = document.createElement("p");
-        p.innerHTML = items[i].author;
+        p.innerHTML = items[keys[i]].score[0];
         news.appendChild(p);
     }
   });
