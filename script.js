bc490b8ba90f3c7a573ee6bf47fcf9ea13346c98
jfauls <129209075+jfauls@users.noreply.github.com>
2023-03-28 21:01:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-fetch("http://jfauls.github.io/mal-scores-over-time/SeasonalAnime.json")
+fetch("https://jfauls.github.io/mal-scores-over-time/SeasonalAnime.json")
   .then((response) => response.json())
   .then((data) => console.log(data));
