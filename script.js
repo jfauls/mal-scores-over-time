aa39130f75e4e5776d7198d3fc43e8a22369733e
Joseph Fauls <hnuisqt@gmail.com>
2023-03-29 18:02:49 -0400

    script change

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 fetch("https://jfauls.github.io/mal-scores-over-time/SeasonalAnime.json")
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((data) => {
+    var news = document.getElementsByClassName("news-story")[0];
+    var items = data;
+    console.log(data)
+    for(var i = 0; i < items.length; i++) {
+        var h5 = document.createElement("h5");
+        h5.innerHTML = items[i].title;
+        news.appendChild(h5);
+        var p = document.createElement("p");
+        p.innerHTML = items[i].author;
+        news.appendChild(p);
+    }
+  });
