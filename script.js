3c7eaaa4f4c7e3d57dfb91843e46394d61b8f2ca
jfauls <129209075+jfauls@users.noreply.github.com>
2023-03-28 20:24:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
-$.getJSON("./SeasonalAnime.json", function (data) {
-    console.log(data);
-  })
+fetch("https://github.com/jfauls/mal-scores-over-time/SeasonalAnime.json")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
